1499e64cf709cf16e3202fab5d71e084e63eed90
kirameki-futurestar <xsasx77@gmail.com>
2020-02-16 16:35:47 +0900

    전장의 안개 최적화
    눈에 보이는게 안좋더라도 퍼포먼스가 괜찮은 쪽으로 선택

--- Assets/Scripts/20.Behaviour/DumbAI.cs
+++ Assets/Scripts/20.Behaviour/DumbAI.cs
@@ -12,31 +12,5 @@ public class DumbAI : MonoBehaviour {
         rotateCommand = new RotateUnitCommand();
     }
 
-    private void Start()
-    {
-        StartCoroutine(Patrol());
-
-    }
-
-
-    IEnumerator Patrol()
-    {
-        float _t = 0;
-        float angle = 0;
-        while (true)
-        {
-            moveCommand.Execute(gameObject);
-
-            if (_t > 3)
-            {
-                angle += 180;
-                rotateCommand.Initialize(angle);
-                rotateCommand.Execute(gameObject);
-                _t = 0;
-            }
-            _t += Time.deltaTime;
-            yield return null;
-        }
-    }
 
 }
